2a921e446a8e430e146697cbadafbf0207d88755
Administrator <510901428@qq.com>
2016-07-18 15:57:05 +0800

    delete a

--- app/src/main/java/com/pro/foru/activity/MainActivity.java
+++ app/src/main/java/com/pro/foru/activity/MainActivity.java
@@ -1,10 +1,10 @@
 package com.pro.foru.activity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
 
-import com.pro.foru.activity.fragment.MainFragment;
-import com.pro.foru.foru.R;
-import com.trello.rxlifecycle.components.support.RxFragmentActivity;
+        import com.pro.foru.activity.fragment.MainFragment;
+        import com.pro.foru.foru.R;
+        import com.trello.rxlifecycle.components.support.RxFragmentActivity;
 
 public class MainActivity extends RxFragmentActivity {
 
